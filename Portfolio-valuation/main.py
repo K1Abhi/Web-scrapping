e3d3a930e71a442e6094053c65c7d8247aa2b2ad
Abhishek kumar <amaterasuabhishek@gmail.com>
2024-03-20 00:59:15 +0530

    p1 little

--- Portfolio-valuation/main.py
+++ Portfolio-valuation/main.py
@@ -5,6 +5,19 @@ from bs4 import BeautifulSoup
 def get_price_information(ticker, exchange):
     url = f"https://www.google.com/finance/quote/{ticker}:{exchange}"
     resp = r.get(url)
-    soup = BeautifulSoup(resp.content)
+    soup = BeautifulSoup(resp.content, "html.parser")
+
+    price_div = soup.find("div", attrs={"data-last-price": True})
+    price = float(price_div["data-last-price"])
+    currency = price_div["data-currency-code"]
+
+    return {
+        "ticker": ticker,
+        "exchange": exchange,
+        "price": price,
+        "currency": currency
+    }
 
 
+if __name__ == "__main__":
+    print(get_price_information("MSFT", "NASDAQ"))
